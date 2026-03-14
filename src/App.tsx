944a3fd8576317d3ea07aac3bda2500d6b8a0fe2
SubrotoEagleinbrit <subroto.chakraborty@eagleinbrit.com>
2026-03-14 12:46:10 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import "./App.css";
 import Button from "./ui/Button";
 import Modal from "./ui/Modal";
 import Form from "./components/Form";
+
 function App() {
   const [open, setOpen] = useState(false);
   return (
     <div className="parent_section">
       <div className="parent">
-        <h1>User Details Modal</h1>
+        <h1 className="heading">User Details Modal</h1>
         <Button onClick={() => setOpen(!open)} label="Open Form" />
 
         <Modal isOpen={open} onClose={() => setOpen(false)}>
